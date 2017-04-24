1cde586874dd4a5da5dab0404d2934757f5ba0ac
tzits <tzitsman@gmail.com>
2017-04-24 17:07:09 -0400

    starts continuous wordcount

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ $(document).ready(function() {
 
 })
 
+setInterval
+
 function count() {
-	if ($('#mainText').val() == '') {
-		$('#count').text(0)
-	} else {
-		var newVal = $('#mainText').val().split(' ').length
-		$('#count').text(newVal)
-		console.log(newVal)
-	}
+	setInterval(function() {
+		console.log('counting')
+		if ($('#mainText').val() == '') {
+			$('#count').text(0)
+		} else {
+			var newVal = $('#mainText').val().split(' ').length
+			$('#count').text(newVal)
+			console.log(newVal)
+		}
+	}, 2000)
+}
+
+function startCount() {
+	console.log('so far so good')
 }
