6667a701fa1c54a45103934052153ecccdbe7ceb
tzits <tzitsman@gmail.com>
2017-04-27 13:43:32 -0400

    adds content to model files

--- public/models/Docs.js
+++ public/models/Docs.js
@@ -1,1 +1,18 @@
-Docs.js
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var User = require('./Users');
+
+var DocSchema = new Schema({
+	title: String,
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	start_count: Number,
+	content: String
+})
+
+var Doc = mongoose.model('Doc', DocSchema)
+
+module.exports = Doc
--- public/models/Index.js
+++ public/models/Index.js
@@ -1,1 +1,8 @@
-Index.js
+var mongoose = require("mongoose");
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/speedwriter")
+
+var Doc = require('./Docs');
+var User = require('./Users');
+
+module.exports.Doc = Doc;
+module.exports.User = User;
