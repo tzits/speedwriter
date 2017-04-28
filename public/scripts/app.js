9ddaaf2a9738a26f49af1e397533dd5e325c744c
tzits <tzitsman@gmail.com>
2017-04-28 16:21:18 -0400

    can change font size

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 	console.log("type away");
+
+	// var text = document.getElementById('mainText')
+	// text.addEventListener('keydown', function(e){
+	// 	if (e.keyCode == 9) {
+	// 		e.preventDefault()
+	// 		console.log('indenting')
+	// 		$('#mainText').append(document.createTextNode('    '));
+	// 	} else if (e.keyCode == 13) {
+	// 		e.preventDefault()
+	// 	}
+	// })
+
 	var h1 = document.getElementsByTagName('h1')[0],
     start = document.getElementById('start'),
     stop = document.getElementById('stop'),
@@ -7,44 +19,49 @@ $(document).ready(function() {
     seconds = 0, minutes = 0, hours = 0,
     t;
 
-function add() {
-    seconds++;
-    if (seconds >= 60) {
-        seconds = 0;
-        minutes++;
-        if (minutes >= 60) {
-            minutes = 0;
-            hours++;
-        }
-    }
-    
-    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+	function add() {
+	    seconds++;
+	    if (seconds >= 60) {
+	        seconds = 0;
+	        minutes++;
+	        if (minutes >= 60) {
+	            minutes = 0;
+	            hours++;
+	        }
+	    }
+	    
+	    h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
-    timer();
-}
-function timer() {
-    t = setTimeout(add, 1000);
-    count()
-}
+	    timer();
+	}
+	function timer() {
+	    t = setTimeout(add, 1000);
+	    count()
+	}
 
 
 
-/* Start button */
-start.onclick = timer;
+	/* Start button */
+	start.onclick = timer;
 
-/* Stop button */
-stop.onclick = function() {
-    clearTimeout(t);
-}
+	/* Stop button */
+	stop.onclick = function() {
+	    clearTimeout(t);
+	}
 
-/* Clear button */
-clear.onclick = function() {
-    h1.textContent = "00:00:00";
-    seconds = 0; minutes = 0; hours = 0;
-}
+	/* Clear button */
+	clear.onclick = function() {
+	    h1.textContent = "00:00:00";
+	    seconds = 0; minutes = 0; hours = 0;
+	}
+
+	$('select').on('change', function() {
+  		console.log(this.value);
+  		var size = parseInt(this.value) + 4
+  		$('#mainText').attr('style','font-size:' + size + 'px')
+	})
 })
 
-setInterval
 
 function count() {
 	setInterval(function() {
@@ -93,4 +110,8 @@ function hide() {
 function show() {
 	$('#timer-container').attr('style','display:inline')
 	$('#show').attr('style','display:none')
+}
+
+function indent() {
+	console.log('indent')
 }
