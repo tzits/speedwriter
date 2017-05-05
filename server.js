841f9431cc4ac73a2a48fd452c9597d6b10bfb22
tzits <tzitsman@gmail.com>
2017-05-04 22:23:06 -0400

    adds server routes

--- server.js
+++ server.js
@@ -40,7 +40,26 @@ app.get('/', function homepage(req, res) {
 /*
  * JSON API ENDPOINTS
  */
+ // Get One Doc
+app.get('/api/users/:user_id/docs/:id', controllers.docs.show)
 
+// Get All Users Doc
+app.get('/api/users/:id/docs', controllers.docs.index)
+
+// Get One User
+app.get('/api/users/:id', controllers.docs.getuser)
+
+// Create User
+app.post('/api/users', controllers.users.createUser)
+
+// Create Document
+app.post('/api/users/:id/docs', controllers.docs.createDoc)
+
+// Update User
+app.patch('/api/users/:id', controllers.users.updateUser)
+
+// Update Document
+app.patch('/api/users/:user_id/docs/:id', controllers.docs.updateDoc)
 
 //server
 app.listen(process.env.PORT || 8000, function() {
