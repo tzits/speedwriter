47923495564eb97811f54d37ee25179ece84c5ba
tzits <tzitsman@gmail.com>
2017-05-13 19:45:32 -0400

    maybe this works now

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,44 +1,6 @@
 var db = require('../models');
 var mongoose = require('mongoose');
-mongoose.connect(dbConfig.url)
-var passport = require('passport');
-var expressSession = require('express-session');
-app.use(expressSession({secret: 'mySecretKey'}));
-app.use(passport.initialize());
-app.use(passport.session());
 
-passport.serializeUser(function(user, done) {
-	done(null, user._id);
-});
-
-passport.deserializeUser(function(id, done) {
-	User.findById(id, function(err, user) {
-		done(err, user);
-	});
-});
-
-passport.use('login', new LocalStrategy({
-	passReqToCallback : true
-},
-function(req, username, password, done) {
-	User.findOne({'username' : username},
-		function(err, user) {
-			if(err)
-				return done(err);
-			if(!user){
-				console.log('User Nof Found with username '+username);
-				return done(null, false,
-					req.flash('message', 'User not Found'))
-			}
-			if (!isValidPassword(user, password)) {
-				console.log('Invalid Password');
-				return done(null, false,
-					req.flash('message','Invalid Password'))
-			}
-			return done(null, user);
-		}
-	);
-}))
 
 function getUser(req, res) {
 	var userId = req.params.id;
