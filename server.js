de316ff89c0e3282cfc72554fa8bd0a6c7fa443c
tzits <tzitsman@gmail.com>
2017-05-13 22:11:01 -0400

    ) are weird...

--- server.js
+++ server.js
@@ -66,6 +66,54 @@ passport.use('login', new LocalStrategy({
     );
 }));
 
+var isValidPassword = function(user, password){
+  return bCrypt.compareSync(password, user.password);
+}
+
+passport.use('signup', new LocalStrategy({
+    passReqToCallback : true
+  },
+  function(req, username, password, done) {
+    findOrCreateUser = function(){
+      // find a user in Mongo with provided username
+      User.findOne({'username':username},function(err, user) {
+        // In case of any error return
+        if (err){
+          console.log('Error in SignUp: '+err);
+          return done(err);
+        }
+        // already exists
+        if (user) {
+          console.log('User already exists');
+          return done(null, false, 
+             req.flash('message','User Already Exists'));
+        } else {
+          // if there is no user with that email
+          // create the user
+          var newUser = new User();
+          // set the user's local credentials
+          newUser.username = username;
+          newUser.password = createHash(password);
+          newUser.firstName = req.param('firstName');
+ 
+          // save the user
+          newUser.save(function(err) {
+            if (err){
+              console.log('Error in Saving user: '+err);  
+              throw err;  
+            }
+            console.log('User Registration succesful');    
+            return done(null, newUser);
+          });
+        }
+      });
+    };
+     
+    // Delay the execution of findOrCreateUser and execute 
+    // the method in the next tick of the event loop
+    process.nextTick(findOrCreateUser);
+  }));
+
 /**********
  * ROUTES *
  **********/
