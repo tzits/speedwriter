a647746a04fcb70baf6ab718408f7c133aad0c30
tzits <tzitsman@gmail.com>
2017-05-17 16:04:44 -0400

    adds angular show ability

---
+++ public/scripts/controllers/DocsShowController.js
@@ -0,0 +1,22 @@
+angular
+  .module("SpeedWriter")
+  .controller('DocsShowController', DocsShowController);
+
+DocsShowController.$inject = ['$http', '$routeParams'];
+	
+function DocsShowController ( $http, $routeParams) {
+	console.log('show me the monkey')
+	console.log($routeParams)
+	var vm = this;
+	console.log($routeParams.id);
+	var docId= $routeParams.id
+	console.log(docId)
+
+	$http({
+	method: 'GET',
+	url: '/api/docs/'+ docId
+	}).then(function successCallback(json) {
+		vm.doc = json.data;
+		console.log(vm.doc)
+	})
+}
