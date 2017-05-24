ce08400f33a93d9a4195ebc2943e597bbc40edd9
tzits <tzitsman@gmail.com>
2017-05-24 16:54:26 -0400

    timer works suckas

--- public/scripts/controllers/DocsEditController.js
+++ public/scripts/controllers/DocsEditController.js
@@ -2,17 +2,39 @@ angular
   .module("SpeedWriter")
   .controller('DocsEditController', DocsEditController);
 
-DocsEditController.$inject = ['$http', '$routeParams', '$scope'];
+DocsEditController.$inject = ['$http', '$routeParams', '$scope', '$interval'];
 	
-function DocsEditController ( $http, $routeParams, $scope) {
+function DocsEditController ( $http, $routeParams, $scope, $interval) {
 	console.log($routeParams)
 	var vm = this;
 	console.log($routeParams.id);
 	var docId= $routeParams.id
 	console.log(docId)
+	var h1 = document.getElementsByTagName('h1')[0],
+    start = document.getElementById('start'),
+    stop = document.getElementById('stop'),
+    clear = document.getElementById('clear'),
+    seconds = 0, minutes = 0, hours = 0,
+    t;
 
 	$scope.log = function() {
 		$scope.logged = "It worked"
+	}
+	$scope.start = function() {
+		$scope.logged = "Timer should start now"
+		timer = $interval(function() {
+	    seconds++;
+	    if (seconds >= 60) {
+	        seconds = 0;
+	        minutes++;
+	        if (minutes >= 60) {
+	            minutes = 0;
+	            hours++;
+		    }
+		}
+		h1.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+		}, 1000)
+
 	}
 
 	$http({
