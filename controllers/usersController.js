95ef1080e71226010bc1335baf61647ff9a24b86
tzits <tzitsman@gmail.com>
2017-05-26 14:33:14 -0400

    cleans code

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -30,45 +30,13 @@ function createUser(req, res) {
 			});
 		}
 	});
-
+	console.log(req.body)
 }
 
-passport.use(new LocalStrategy({
-    usernameField: 'email'
-  },
-  function(username, password, done) {
-    User.findOne({ email: username }, function (err, user) {
-      if (err) { return done(err); }
-      // Return if user not found in database
-      if (!user) {
-        return done(null, false, {
-          message: 'User not found'
-        });
-      }
-      // Return if password is wrong
-      if (!user.validPassword(password)) {
-        return done(null, false, {
-          message: 'Password is wrong'
-        });
-      }
-      // If credentials are correct, return the user object
-      return done(null, user);
-    });
-  }
-));
-
-
 function login(req,res) {
-	var user = User.findOne({email: req.body.email}, function(err, user) {
+	console.log(req.body,'look at me')
+	User.findOne({email: req.body.email}, function(err, user) {
 		if (err) {console.log(err)}
-		// else {
-		// 	if (user.hash == anotherUser.hash) {
-		// 		console.log(huzzah)
-		// 	} else {
-		// 		console.log('bad password',user.hash,anotherUser.hash)
-		// 	}
-			
-		// }
 		console.log(user)
 		console.log(user.validPassword(req.body.password))
 	})
