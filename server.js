c5e85dc4a54c4b87798d48dc6183cc63e41a6da2
Toby <tzitsman@gmail.com>
2017-07-08 23:47:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ var db = require('./models');
 var User = db.User
 
 //Passport
+// not working at the moment, can't figure out why
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var expressSession = require('express-session');
